a65f822bc363504faaec719c17a725f2455f8428
unknown <Administrador@UTP230844.grupoutp.net>
2021-04-21 01:08:36 -0500

    primera impresion

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import CardsPerfil from './Compomentes/CardsPerfil.js';
+
+class App extends React.Component {
+  render(){
+    return(
+      <div className="container">
+          <div className=" row col-md-12 p-4 p-md-5 mb-4 text-white rounded bg-dark">
+            <div className="col-md-4 px-0">
+              <CardsPerfil />
+            </div>
+            <div className="col-md-1 px-0"></div>
+            <div className="col-md-7 px-0">
+              <h1 className="display-4 fst-italic">Title of a longer featured blog post</h1>
+              <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
+              <p className="lead mb-0"><a href="/" className="text-white fw-bold">Continue reading...</a></p>
+            </div>
+          
+        </div>
+        
+      </div>
+    )
+  }
 }
 
 export default App;
